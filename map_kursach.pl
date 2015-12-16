3fe49f73ecd5087c81847fc1d6f3a8f7debfb264
Tunnikov Vlad <tunnikov.vlad@gmail.com>
2015-12-16 16:03:18 +0300

    добавил роутер, мелкие исправления

--- map_kursach.pl
+++ map_kursach.pl
@@ -6,6 +6,7 @@ use GraphViz;
 use List::MoreUtils qw/uniq/;
 
 my @hosts = qw (192.168.11.13 192.168.11.16);
+my $router = "192.168.11.11";
 my $href;
 my %res;
 my @mac;
@@ -13,14 +14,44 @@ my @port;
 my $i;
 my $g = GraphViz->new();
 my $oid;
+my %ips;
+my @ip;
+my ($ses, $err) = Net::SNMP->session(
+		-hostname	=>	$router,
+		-version	=>	"2",
+		-community	=>	"public"
+	);
+	%res = %{$ses->get_table(".1.3.6.1.2.1.4.22.1.2.2")};
+
+#print Dumper \%res;
+my $c=0;
+my $tmp;
+my @atmp;
+foreach (keys(%res)) { #снятие с роутера ип->мак
+#	$_ =~ /(.1.3.6.1.2.1.4.22.1.2.2.)(.*)/;
+	$c = substr($_, 24);
+	$tmp = substr($res{$_}, 2);
+	@atmp = ($tmp =~ m/.{2}/g );
+	$tmp = join("-", @atmp);
+#	$res{$_} = $tmp;
+	$ips{$c} = $tmp;
+}
+$ips{"192.168.11.11"} = "00-05-5d-78-de-16";
+#print Dumper \%ips;
+
 foreach my $ip (@hosts) {
 	my ($ses, $err) = Net::SNMP->session(
 		-hostname	=>	$ip,
 		-version	=>	"2",
 		-community	=>	"public"
 	);
-	%res = %{$ses->get_table(".1.3.6.1.2.1.17.7.1.2.2.1.2")};
+
+
+
+	%res = %{$ses->get_table(".1.3.6.1.2.1.17.7.1.2.2.1.2")}; #mac->port
 #	%res = uniq %res; #убирает только ключи, маки остаются,но криво - используются как ключи
+
+	#print Dumper \%res;
 
 	my %hash;
 		foreach my $mac (values %res) { #порты с кол-вом повторений
@@ -49,46 +80,31 @@ foreach my $ip (@hosts) {
 
 	my $c;
 	my %hex_mac;
+#	print Dumper \%res;
 	foreach (keys(%res)) {
-	#print "$_ -> $res->{$_}\n";
-		$_ =~ /(.1.3.6.1.2.1.17.7.1.2.2.1.2.1.)(.*)/;
-		@mac = split(/\./, $2);
+		@mac = split(/\./, substr($_, 30));
 		$c = 0;
 		foreach my $i (@mac) {
 			$mac[$c] = sprintf ("%x", $i);# "$i\n";
+			if (length($mac[$c]) == 1) {$mac[$c] = "0".$mac[$c];}#print $mac[$c], "\n";}
 			$c++;
 		}
 		$hex_mac{join("-",@mac)} = $res{$_};
 	}
-
-	if ($ip eq "192.168.11.13") {
-		$oid = ".1.3.6.1.2.1.3.1.1.2.22.1";
-	}
-	else {$oid = ".1.3.6.1.2.1.3.1.1.2.30.1";}
-	
-	%res = %{$ses->get_table($oid)};
-	print Dumper \%res;
-	foreach (keys(%res)) {
-		$_ =~ /(.1.3.6.1.2.1.3.1.1.2.22.1)(.*)/;
-		print $_, "\n";
-	}
-
+	#print Dumper \%hex_mac;
+my $flag;
 	$g->add_node("$ip");
 	foreach my $mac (sort {$hex_mac{$a} <=> $hex_mac{$b}} keys %hex_mac) {#сортировка портов
-		$g->add_node("$mac");
-		$g->add_edge("$mac" => "$ip", label => "$hex_mac{$mac}");
-#	print "$hex{$hex_mac}\n";
+		$flag = 0;		
+		foreach (keys %ips){
+			if ($ips{$_} eq $mac) {
+				$g->add_edge("$mac\n$_" => "$ip", label => "$hex_mac{$mac}");
+				$flag = 1;
+			}
+		}
+	if ($flag == 0) {$g->add_edge("$mac" => "$ip", label => "$hex_mac{$mac}");}
 	}
-
-#	foreach my $hex_mac (keys %res) {print "$hex_mac -> $res{$hex_mac}\n";}
 #print Dumper \%res;
-
-
-#	print "\n";
-
 }
-
-#print $g->as_png;
-#выдаются 22 и 7 порты 13 комм в выдаче у 16 комм
-#у 13 30 порт 16 и ещё хз какой 
-
+$g->add_edge("192.168.11.16" => "192.168.11.13");
+print $g->as_png;
